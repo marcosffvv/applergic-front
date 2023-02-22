0f7c8ea762058913f971636c6542a0232270feb9
marcosffvv <marcosfrailefdz02@gmail.com>
2023-02-22 08:56:02 +0100

    last dance

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -1,23 +1,10 @@
 import "./GalleryPage.scss";
-// import { Link } from "react-router-dom"
-import { useContext, useEffect, useState} from "react"
-import { JwtContext } from "../../shared/contexts/JwtContext"
-import { API } from "../../shared/services/api"
-import calendar from "../../assets/calendario.png"
-import filter from "../../assets/filter.png"
-import edit from "../../assets/edit.png"
-import CrossComponet from '../../components/CrossComponet/CrossComponent'
-// import ButtonComponent from '../../components/ButtonComponent/ButtonComponent'
-
+import { useEffect, useState} from "react";
+import { API } from "../../shared/services/api";
+import CrossComponet from '../../components/CrossComponet/CrossComponent';
 
 export default function GalleryPage() {
   const [products, setProducts] = useState([]);
-  // //
-  // const [editing, setEditing] = useState(false);
-  // const [editNotes, setEditNotes] = useState('');
-  // //
-  const {newUser, setUser} = useContext(JwtContext);
-  // console.log("nuevousauario", JSON.parse(newUser));
 
   useEffect(() => {
     const getProducts = () => {
@@ -29,41 +16,39 @@ export default function GalleryPage() {
   }, []);
 
   return(
-      <div className="gallery">
-
-        editdiary
-        <div className='gallery__cross'>  
-            <CrossComponet ruta={'/home'}></CrossComponet>
-        </div>
-
-        <div className='gallery__tittle'>
-           <p className='tittle-Bold'>Galería de productos</p>
-        </div>
-
-        <div className='gallery__products'>
-          {products.map((item, index) => (
-            <div key={index} className="gallery__products__div">
-
-              <div className='gallery__products__div--img'>
-                <img className='gallery__products__div--img-1' src={item.img} alt={item.name}/>
-              </div>
-
-              <div className='gallery__products__div--card'>
-                <h3 className="dates">{item.name}</h3>
-                <p>Marca: {item.brand}</p>
-                <p>EAN: {item.EAN}</p>
-                <br></br>
-                <h4>Ingredientes:</h4>
-                {item.components.map((component, i) => (
-                  <ul key={i}>
-                    <li>{component.name}</li>
-                  </ul>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-
+    <div className="gallery">
+      <div className='gallery__cross'>  
+          <CrossComponet ruta={'/home'}></CrossComponet>
       </div>
+
+      <div className='gallery__tittle'>
+          <p className='gallery__tittle--bold'>Galería de productos</p>
+      </div>
+
+      <div className='gallery__products'>
+        {products.map((item, index) => (
+          <div key={index} className="gallery__products__div">
+
+            <div className='gallery__products__div--img'>
+              <img className='gallery__products__div--img-1' src={item.img} alt={item.name}/>
+            </div>
+
+            <div className='gallery__products__div--card'>
+              <h3 className='gallery__products__div--card--h3'>{item.name}</h3>
+              <p className='gallery__products__div--card--p'><span>Marca: </span>{item.brand}</p>
+              <p className='gallery__products__div--card--p'><span>EAN: </span>{item.EAN}</p>
+              <br></br>
+              <h4 className='gallery__products__div--card--h4'>Ingredientes:</h4>
+              {item.components.map((component, i) => (
+                <ul key={i}>
+                  <li>{component.name}</li>
+                </ul>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      
+    </div>
   )
 }
